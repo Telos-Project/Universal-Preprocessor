a8aa7c44c4016d4856bfee9466d00e736e1d8295
Jesse Dinkin <kaeon.ace@gmail.com>
2025-06-20 15:27:19 -0400

    Update Code

--- Code/CMD/pupCMD.js
+++ Code/CMD/pupCMD.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
-require("use-js");
+require("telos-use-js");
 
-var universalPreprocessor = use("./universalPreprocessor.js");
+var fs = require("fs");
+var universalPreprocessor = require("./UniversalPreprocessor.js");
 
-let langs = "./pupLangs.js";
+let langs = require("./pupLangs.js");
 
-// STUB
+if(process.argv[4] != null && process.argv[4] != "null")
+	langs = use(process.argv[4]);
+
+let result = universalPreprocessor.preprocess(
+	langs,
+	fs.readFileSync(process.argv[2], "utf-8"),
+	process.argv.length > 5 ? process.argv.slice(5) : []
+);
+
+fs.writeFileSync(
+	process.argv[3],
+	typeof result == "string" ? result : new Uint8Array(Buffer.from(result))
+);
